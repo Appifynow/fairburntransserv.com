a700b653f1001b31bf1f8ba67001cb1e0726091b
dcoollx <dsqjr@msn.com>
2026-05-15 11:25:16 -0400

    add baseURL

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fairburntransserv.com/',
   plugins: [react(), tailwindcss(),],
   define: {
     VITE_API_URL: process.env.VITE_API_URL 
