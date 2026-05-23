f736becf3215f3fc7ee37e9c9b04159474e619b6
dcoollx <dsqjr@msn.com>
2026-05-23 10:40:29 -0400

    fixed map

--- apps/frontend/src/components/StaticMap.tsx
+++ apps/frontend/src/components/StaticMap.tsx
@@ -1,7 +1,7 @@
 export const StaticMap = () => {
     return (
         <a href="https://www.google.com/maps/place/8521+Hospital+Dr,+Douglasville,+GA+30134/@33.7484037,-84.7338378,17z/data=!3m1!4b1!4m6!3m5!1s0x88f526f5818e029f:0x8421d2e505ace159!8m2!3d33.7483993!4d-84.7312629!16s%2Fg%2F11c2h0ddr2?entry=ttu&g_ep=EgoyMDI2MDUxNy4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer">
-       <img src="/public/map.png" alt="Map showing location of Fairburn Transmission Service" className="w-full rounded-lg border-4 border-red-600 mb-8" />
+       <img src={`${import.meta.env.BASE_URL}public/map.png`} alt="Map showing location of Fairburn Transmission Service" className="w-full rounded-lg border-4 border-red-600 mb-8" />
        <span className="sr-only">Opens Google Maps in a new tab</span>
          </a>
     );
